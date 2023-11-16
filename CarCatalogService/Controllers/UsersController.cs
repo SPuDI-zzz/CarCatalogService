a4c052aee1b711f9d76e6f60670e3fda755fd7ee
SPuDI-zzz <danillubavin1gmail.com>
2023-11-16 17:40:15 +0300

    add users controller and views

---
+++ CarCatalogService/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using CarCatalogService.Services.RoleService;
+using CarCatalogService.Services.UserService;
+using CarCatalogService.Services.UserService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CarCatalogService.Controllers
+{
+    public class UsersController : Controller
+    {
+        private protected IUserSevice _userService;
+        private readonly IRoleService _roleService;
+
+        public UsersController(IUserSevice userService, IRoleService roleService)
+        {
+            _userService = userService;
+            _roleService = roleService;
+        }
+
+        public async Task<IActionResult> Index()
+        {            
+            var response = await _userService.GetAllUsers();
+            return View(response);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var roles = await _roleService.GetAllRoles();
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AddUserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var roles = await _roleService.GetAllRoles();
+                ViewBag.Roles = new SelectList(roles, "Id", "Name");
+                return View(userModel);
+            }
+            await _userService.AddUser(userModel);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ CarCatalogService/Views/Users/Models/UserModelView.cs
@@ -0,0 +1,10 @@
+﻿using CarCatalogService.Services.RoleService.Models;
+using CarCatalogService.Services.UserService.Models;
+
+namespace CarCatalogService.Views.Users.Models;
+
+public class UserModelView
+{
+    public IEnumerable<UserModel> Users { get; set; }
+    public IEnumerable<RoleModel> Roles { get; set; }
+}
