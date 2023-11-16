d91a619467331f597449f969b24a31b143ae43e9
SPuDI-zzz <danillubavin1gmail.com>
2023-11-16 18:53:09 +0300

    add edit in controller and view

--- CarCatalogService/Controllers/CarsController.cs
+++ CarCatalogService/Controllers/CarsController.cs
@@ -1,5 +1,7 @@
-﻿using CarCatalogService.Services.CarService;
+﻿using AutoMapper;
+using CarCatalogService.Services.CarService;
 using CarCatalogService.Services.CarService.Models;
+using CarCatalogService.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarCatalogService.Controllers;
@@ -7,9 +9,11 @@ namespace CarCatalogService.Controllers;
 public class CarsController : Controller
 {
     private readonly ICarService _carService;
-    public CarsController(ICarService carService)
+    private readonly IMapper _mapper;
+    public CarsController(ICarService carService, IMapper mapper)
     {
         _carService = carService;
+        _mapper = mapper;
     }
 
     public async Task<IActionResult> Index()
@@ -31,6 +35,32 @@ public class CarsController : Controller
             return View(carModel);
         }
         await _carService.AddCar(carModel);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Edit(long id)
+    {
+        var car = await _carService.GetCar(id);
+        if (car == null)
+            return View("Error");
+
+        var carViewModel = _mapper.Map<EditCarViewModel>(car);
+        return View(carViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(long id, EditCarViewModel carViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            ModelState.AddModelError("", "Failed to edit car");
+            return View(nameof(Edit), carViewModel);
+        }
+
+        var carModel = _mapper.Map<UpdateCarModel>(carViewModel);
+
+        await _carService.UpdateCar(id, carModel);
+
         return RedirectToAction(nameof(Index));
     }
 }
---
+++ CarCatalogService/ViewModels/EditCarViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CarCatalogService.Services.CarService.Models;
+
+namespace CarCatalogService.ViewModels;
+
+public class EditCarViewModel
+{
+    public long Id { get; set; }
+    public required string Mark { get; set; }
+    public required string Model { get; set; }
+    public required string Color { get; set; }
+    public long UserId { get; set; }
+}
+
+public class EditCarViewModelProfile : Profile
+{
+    public EditCarViewModelProfile()
+    {
+        CreateMap<CarModel, EditCarViewModel>();
+        CreateMap<EditCarViewModel, UpdateCarModel>();
+    }
+}
