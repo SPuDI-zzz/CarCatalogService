8813d95a20654aed303c8b0b89ba64ef34f7f328
SPuDI-zzz <danillubavin1gmail.com>
2023-11-17 17:47:45 +0300

    add account controller

---
+++ CarCatalogService/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using CarCatalogService.Data.Entities;
+using CarCatalogService.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarCatalogService.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // TODO
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(loginViewModel);
+
+            return View();
+        }
+    }
+}
