f62be757bb693dd4bce420cf6e26263b82a39bc6
SPuDI-zzz <danillubavin1gmail.com>
2023-11-23 15:03:06 +0300

    test with russian symbols

---
+++ CarCatalogService/BLL/Services/AccountService/IAccountService.cs
@@ -0,0 +1,31 @@
+﻿using CarCatalogService.BLL.Services.AccountService.Models;
+
+namespace CarCatalogService.BLL.Services.AccountService;
+
+/// <summary>
+///     Defines the contract for a service responsible for user account management operations.
+/// </summary>
+public interface IAccountService
+{
+    /// <summary>
+    ///     Asynchronously registers a new user account based on the provided information.
+    /// </summary>
+    /// <param name="model">
+    ///     A <see cref="RegisterUserAccountModel"/> containing the user's registration details.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="Task"/> representing the asynchronous operation.
+    /// </returns>
+    Task Register(RegisterUserAccountModel model);
+
+    /// <summary>
+    /// Тест Тест
+    ///     Asynchronously performs user authentication based on the provided login information.
+    /// </summary>
+    /// <param name="model">A <see cref="LoginUserAccountModel"/> containing the user's login details.</param>
+    /// <returns>
+    ///     A <see cref="Task"/> representing the asynchronous operation.
+    ///     The task result is a string containing an authentication token if login is successful.
+    /// </returns>
+    Task<string> Login(LoginUserAccountModel model);
+}
